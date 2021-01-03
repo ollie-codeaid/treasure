a1b20626d9c2951c0ce424ad664db3ba147b5d53
Ollie <reelfeet@hotmail.com>
2021-01-03 13:25:16 +0000

    First commit with Grommet getting started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import {
+  Button,
+  Box,
+  Form,
+  FormField,
+  Grommet,
+  Heading,
+  Text,
+  TextInput,
+} from 'grommet';
+
+const theme = {
+  global: {
+    colors: {
+      theme: '#660080',
+    },
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
 
 function App() {
+  const [value, setValue] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grommet theme={theme} full>
+      <Box
+        pad='large'
+        animation={{ type: 'fadeIn', duration: 8000 }}
+        align='center'
+      >
+        <Box align='center' justify='center'>
+          <Heading textAlign='center'>Welcome to the treasure hunt!</Heading>
+          <Text>Description text about th</Text>
+          <Text>Description text about th</Text>
+        </Box>
+        <Form
+          onChange={nextValue => setValue(nextValue)}
+          onSubmit={event => console.log(event)}
+          align='center'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <FormField name='answer' required>
+            <TextInput name='answer' type='text' />
+          </FormField>
+          <Button type='submit' label='submit' primary />
+        </Form>
+      </Box>
+    </Grommet>
   );
 }
 
